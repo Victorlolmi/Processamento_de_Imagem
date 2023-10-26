4ee506ebe45dfe5ef8e2668700e8a591be4515d7
VICTOR EMMANUEL OLMI LOUZADA <89552640+Victorlolmi@users.noreply.github.com>
2023-10-26 19:41:40 -0300

    topico8

---
+++ Topico8/filtering.py
@@ -0,0 +1,82 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+def showImages(imgsArray, titlesArray, size, grid=(1,1)):
+
+    y, x = grid
+    fig, axes = plt.subplots(y, x, figsize = size)
+    axes = axes.ravel() if isinstance(axes, np.ndarray) else np.array([axes])
+
+    if len(imgsArray) != len(titlesArray):
+        print("Error: the numbers of images and titles has to be the same!")
+        return
+    
+    for idx, (img,title) in enumerate(zip(imgsArray, titlesArray)):
+        if len(img.shape) == 2: # the image is gray tones
+            axes[idx].imshow(img, cmap='gray')
+        else : # the image is RGB
+            axes[idx].imshow(img)
+        axes[idx].set_title(title, fontdict={'fontsize':18, 'fontweight': 'medium'}, pad=10)
+        if len(title) == 0:
+            axes[idx].axis('off')
+
+    plt.tight_layout() 
+    plt.show()
+
+
+
+def filtering(gray_image, filter_name):
+
+    if filter_name == 'average':
+        blur_img = cv2.blur(gray_image,(9,9))
+        
+        showImages([gray_image, blur_img], ['Gray','Blur image'], size=(7,5), grid=(1,2))
+    
+    elif filter_name == 'gaussian':
+        blur_img = cv2.GaussianBlur(gray_image,(9,9),0)
+        showImages([gray_image, blur_img], ['Gray','GaussianBlur image'], size=(7,5), grid=(1,2))
+        
+    elif filter_name == 'median':
+        median_img = cv2.medianBlur(gray_image, 11)
+        showImages([gray_image, median_img], ['Gray','Median image'], size=(7,5), grid=(1,2))
+
+    elif filter_name== 'sobel':
+        img_sobelx = cv2.Sobel(src= gray_image, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=3 )
+        img_sobely = cv2.Sobel(src= gray_image, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=3 )
+        img_sobelxy = cv2.addWeighted(img_sobelx, 0.5, img_sobely, 0.5, 0)
+
+        imgx = cv2.convertScaleAbs(img_sobelx)
+        imgy = cv2.convertScaleAbs(img_sobely)
+        imgxy = cv2.convertScaleAbs(img_sobelxy)
+
+
+        showImages([gray_image, imgx, imgy, imgxy],['Gray','Sobel x','Sobel y','Sobel xy'], size=(10,10), grid=(2,2))
+
+    elif filter_name == 'laplacian':
+
+        laplacian_img = cv2.Laplacian(gray_image, cv2.CV_64F, ksize=3)
+
+        blur_img = cv2.blur(gray_image,(5,5))
+
+        blur_more_laplacian_img = cv2.Laplacian(blur_img, cv2.CV_64F, ksize=3)
+
+        laplacian_img = cv2.convertScaleAbs(laplacian_img)
+        blur_more_laplacian_img = cv2.convertScaleAbs(blur_more_laplacian_img)
+
+        showImages([gray_image, laplacian_img, blur_more_laplacian_img], ['Original', 'Laplacian', 'Blur + Laplacian'], size=(10,7), grid=(1,3))
+
+    elif filter_name == 'canny_edge':
+        cannyimg = cv2.Canny(gray_image, 30, 255)
+        
+        showImages([gray_image, cannyimg], ['Original', 'Canny Edge'], size=(10,7), grid=(1,2))
+    else:
+        print("Digite um filtro existente:")
+        print("average, gaussian, median, sobel, laplacian, canny_edge")
+
+
+image = cv2.imread("Topico8\imgs\img_icons.png",0)
+
+filter_name = "canny_edge"
+
+filtering(image, filter_name)
---
+++ Topico8/filters.py
@@ -0,0 +1,142 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+def showImages(imgsArray, titlesArray, size, grid=(1,1)):
+
+    y, x = grid
+    fig, axes = plt.subplots(y, x, figsize = size)
+    axes = axes.ravel() if isinstance(axes, np.ndarray) else np.array([axes])
+
+    if len(imgsArray) != len(titlesArray):
+        print("Error: the numbers of images and titles has to be the same!")
+        return
+    
+    for idx, (img,title) in enumerate(zip(imgsArray, titlesArray)):
+        if len(img.shape) == 2: # the image is gray tones
+            axes[idx].imshow(img, cmap='gray')
+        else : # the image is RGB
+            axes[idx].imshow(img)
+        axes[idx].set_title(title, fontdict={'fontsize':18, 'fontweight': 'medium'}, pad=10)
+        if len(title) == 0:
+            axes[idx].axis('off')
+
+    plt.tight_layout() 
+    plt.show()
+
+def blurFilter(image):
+
+    blur_img = cv2.blur(image,(5,5))
+
+    showImages([image, blur_img], ['Original', 'Borrada'], size=(10,10), grid=(1,2))
+
+    return blur_img
+
+def gaussianFilter(image):
+
+    blur_img = cv2.GaussianBlur(image,(5,5),0)
+
+    showImages([image, blur_img], ['Original', 'Borrada'], size=(10,10), grid=(1,2))
+
+    return blur_img
+    
+def medianFilter(image):
+
+    median_img = cv2.medianBlur(image, 11)
+
+    showImages([image, median_img], ['Original', 'Median Filter'], size=(10,10), grid=(1,2))
+
+    return median_img
+
+def sobelFilter(gray_image):
+
+    img_sobelx = cv2.Sobel(src= gray_image, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=3 )
+    img_sobely = cv2.Sobel(src= gray_image, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=3 )
+    #img_sobelxy = cv2.Sobel(src= gray_image, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=3 )
+    img_sobelxy = cv2.addWeighted(img_sobelx, 0.5, img_sobely, 0.5,0)
+
+    return img_sobelx, img_sobely, img_sobelxy
+
+def laplacianFilter(gray_image):
+
+    laplacian_img3 = cv2.Laplacian(gray_image, cv2.CV_64F, ksize=3)
+
+    blur_img = blurFilter(gray_image)
+
+    laplacian_img5 = cv2.Laplacian(gray_image, cv2.CV_64F, ksize=5)
+    
+    blur_more_laplacian_img3 = cv2.Laplacian(blur_img, cv2.CV_64F, ksize=3)
+
+    blur_more_laplacian_img5 = cv2.Laplacian(blur_img, cv2.CV_64F, ksize=5)
+    
+    return laplacian_img3, laplacian_img5, blur_more_laplacian_img3, blur_more_laplacian_img5
+
+def cannyFilter(gray_img, min, max):
+
+    cannyimg = cv2.Canny(gray_img, min, max)
+
+    return cannyimg
+#filtragem passa-Baixa(suavizacao) Retem a alta frequencia
+#passa so a baixa frequencia( onde nao se tem muita variacao de cor)
+
+#filtro da media(blur)
+'''
+img = cv2.imread("Topico8\imgs\img_icons.png")
+
+blur = blurFilter(img)
+
+#filtroGaussiano
+
+gaussian = gaussianFilter(img)
+
+showImages([img, blur, img, gaussian], ('Original', 'Blur por media', 'Original', 'Gaussian Blur'), size=(10,10), grid=(2,2))
+
+#Filtro de Mediana
+
+noise_img = cv2.imread("Topico8\imgs\img_flowers.jpg")
+
+medianFilter(noise_img)
+
+wall_img = cv2.imread("Topico8\imgs\img_parede.jpg")
+original=wall_img
+wall_img = cv2.cvtColor(wall_img, cv2.COLOR_BGR2GRAY)
+imgx, imgy, imgxy = sobelFilter(wall_img)
+wall_img = cv2.cvtColor(original, cv2.COLOR_BGR2RGB)
+
+showImages([wall_img, imgx, imgy, imgxy],['Original','Sobelx','Sobely','Sobelxy'], size=(10,10), grid=(2,2))
+
+#transforma os pixels de negativos para a escala de 0 a 255
+imgx = cv2.convertScaleAbs(imgx)
+imgy = cv2.convertScaleAbs(imgy)
+imgxy = cv2.convertScaleAbs(imgxy)
+
+
+showImages([wall_img, imgx, imgy, imgxy],['Original','Sobelx','Sobely','Sobelxy'], size=(10,10), grid=(2,2))
+'''
+'''
+#laplaciano 
+
+wall_img = cv2.imread("Topico8\imgs\img_icons.png")
+original=wall_img
+wall_img = cv2.cvtColor(wall_img, cv2.COLOR_BGR2GRAY)
+
+laplacian_img3, laplacian_img5, blur_more_laplacian_img3, blur_more_laplacian_img5 = laplacianFilter(wall_img)
+
+laplacian_img3 = cv2.convertScaleAbs(laplacian_img3)
+laplacian_img5 = cv2.convertScaleAbs(laplacian_img5)
+blur_more_laplacian_img3 = cv2.convertScaleAbs(blur_more_laplacian_img3)
+blur_more_laplacian_img5 = cv2.convertScaleAbs(blur_more_laplacian_img5)
+wall_img = cv2.cvtColor(original, cv2.COLOR_BGR2RGB)
+showImages([wall_img, laplacian_img3, blur_more_laplacian_img3, laplacian_img5, blur_more_laplacian_img5], ['Original', 'Laplacian 3', 'Blur + Laplacian 3', 'Lapacian 5', 'Blur + Laplacian 5'], size=(10,7), grid=(2,3))
+
+'''
+
+#CannyEdga
+wall_img = cv2.imread("Topico8\imgs\img_parede.jpg")
+original=wall_img
+wall_img = cv2.cvtColor(wall_img, cv2.COLOR_BGR2GRAY)
+
+
+canny_img = cannyFilter(wall_img, min=175,max = 255)
+wall_img = cv2.cvtColor(wall_img, cv2.COLOR_GRAY2RGB)
+showImages([wall_img, canny_img], ['Original', 'Canny Edga'], size=(10,7), grid=(1,2))
